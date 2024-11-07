8a1b3312b7e605c8c6774755514ff0d146b2b7f7
JulioCabezaMartinez <julio.cabeza.martinez@gmail.com>
2024-11-07 09:44:08 +0100

    putos p's

--- src/view/templates/head.inc.php
+++ src/view/templates/head.inc.php
@@ -2,6 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/CSS/style.css">
+    <link rel="stylesheet" href="../../assets/CSS/preparacion.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/6af6746610.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
